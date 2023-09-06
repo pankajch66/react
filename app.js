03c01e69d2d2f7ca870f77d66ebbc2cffd35342d
pankajch66 <pankajch66@gmail.com>
2023-09-06 23:30:45 +0530

    finally react installed

---
+++ app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const main = React.createElement("h1",{},"Hello this is world");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(main);
---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,5 @@
+const main = React.createElement("h1", {}, "Hello this is world");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(main);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
