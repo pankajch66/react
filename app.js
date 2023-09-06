41ac4b32c7f5f40e05759a2cd9e1d718a8e1b30c
pankajch66 <pankajch66@gmail.com>
2023-09-06 23:46:47 +0530

    update in episode 3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const main = React.createElement("h1",{},"Hello this is world");
+const main = React.createElement("h1",{id:"heading"},"Hello i this is ");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(main);
