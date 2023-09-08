d710843e857261a426484707ee72f2cb7110da44
pankajch66 <pankajch66@gmail.com>
2023-09-08 23:39:22 +0530

    class 3 notes and some practices

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const main = React.createElement("h1",{id:"heading"},"Hello i this is ");
+// const main = React.createElement("h1",{id:"heading"},"Hello i this is ");
+
+/*react element
+const jsxmain = <h1 className="pankaj">hello thi is jsx</h1>
+*/
+// const jsxmain = <h1 className="pankaj">hello thi is jsx</h1> 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxmain);
+
+const hear = (
+    <h2>how are you
+    </h2>
+);
+
+
+const Heading = () => (
+    <div id="container">
+    {hear}
+    <h1>Namaste react</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(main);
+root.render(<Heading/>);
